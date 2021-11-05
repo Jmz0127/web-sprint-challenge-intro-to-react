970297a68b516254c9bdf5906ad43be5d22a5bc1
Jmz0127 <jmezz1@icloud.com>
2021-11-05 11:58:13 -0400

    added character component and passed props through to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; //added useState and useEffect
 import './App.css';
+import axios from 'axios'; //added axios as we are using that to get API info
+
+
+// importing component
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState ([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(resp => {
+      setPeople(resp.data.people);
+    })
+    .catch(err => console.error(err))
+  }, [])
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character people={people}/>
     </div>
   );
 }
---
+++ src/components/Characters.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Character from './Character';
+import styled from 'styled-components';
+
+// add styling here
+const StyledCharacters = styled.div`
+
+
+
+`
+
+//three elements of passing props: import react from react, a const variable with props and return, and then export default
+const Characters = (props) => {
+    return (
+        <div> {/* change this div to StyledCharacters once styling is added */}
+            { props.peoples.map(people => (
+                <Character people={people} key={people.name} /> 
+            )) }
+        </div>
+    )
+}
+
+
+export default Characters;
